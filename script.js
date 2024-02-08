0b74e59f843d054998ccd5a381c091033992af31
Vince Andrew Loverez <155804641+vinceandrewloverez@users.noreply.github.com>
2024-02-08 11:58:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function change(color) {
+    var box = document.querySelector(".box-cont");
+    box.style.borderColor = color;
+    box.style.marginTop = "0px"; // Slide down by setting margin-top to 0
+}
